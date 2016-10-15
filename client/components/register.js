72b451d554f540cf84b05be6ee65cb116dd39009
jeffzhangg <zhang.jeffreyd@gmail.com>
2016-10-15 03:22:32 -0400

    login component

--- client/components/register.js
+++ client/components/register.js
@@ -1,34 +1,46 @@
 import React from 'react';
 
-export default class Login extends React.Component {
+export default class Register extends React.Component {
   constructor(props) {
     super(props);
     this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handlePasswordConfirmedChange = this.handlePasswordConfirmedChange.bind(this);
+    this.passwordMatch = this.passwordMatch.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  getInitialState() {
-    return { username: '' };
-  }
-
   handleUsernameChange(e) {
-    this.setState({username: e.target.value });
+    this.setState({username: e.target.value});
   }
 
   handlePasswordChange(e) {
-    this.setState({password: e.target.value });
+    this.setState({password: e.target.value});
   }
+
+  handlePasswordConfirmedChange(e) {
+    this.setState({confirmedPassword: e.target.value})
+  }
+
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log(this.state.username);
-    console.log(this.state.username);
 
-    Accounts.createUser({
-      username: this.state.username,
-      password: this.state.password
-    });
+    if (this.passwordMatch(this.state.password, this.state.confirmedPassword)) {
+      console.log('passwords match');
+      Accounts.createUser({
+        username: this.state.username,
+        password: this.state.password
+      });
+    } else {
+      console.log('passwords do not match')
+    }
+  }
+
+  passwordMatch(password, confirmedPassword) {
+    if (password === confirmedPassword) {
+      return true;
+    }
   }
 
   render() {
@@ -44,6 +56,12 @@ export default class Login extends React.Component {
           type="text"
           placeholder="password"
           onChange= {this.handlePasswordChange}
+        />
+
+        <input
+          type="text"
+          placeholder="confirm password"
+          onChange= {this.handlePasswordConfirmedChange}
         />
 
         <input
