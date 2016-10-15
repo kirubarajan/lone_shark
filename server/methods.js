89b04a63e94ce763cf62650e3ed3e692c1251d0d
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2016-10-15 06:51:22 -0400

    added functions

--- server/methods.js
+++ server/methods.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 
 Meteor.methods({
 
@@ -6,7 +7,7 @@ Meteor.methods({
 
     // inserts profile shit
 
-    return Profiles.insert({user: user, name: name});
+    return Profiles.insert({user: user, name: name, score: 0});
 
   },
 
@@ -14,22 +15,51 @@ Meteor.methods({
 
     // matching algorithm -> sends matched sender a loan request
 
-    Requests.insert({receiver: receiver, amount: amount});
+    var date = new Date();
+
+    Requests.insert({receiver: receiver, amount: amount, accept: false, open: true, sender: null, time: date.getTime(), score: null});
 
   },
 
-  transfer: function(request, decision) {
+  transfer: function(request, decision, sender) {
 
     // uses blockchain to transfer bitcoin of given request, or discard request
 
     if (decision == true) {
 
-      Requests.update({_id: request}, {open: false, accept: true});
+      Requests.update({_id: request}, {accept: true, sender: sender});
 
     }
+
+    // sends money
 
     var request = Requests.findOne(request);
 
   },
+
+  return: function(request, receiver) {
+
+    var initialTime = Requests.findOne(request).time;
+    var date = new Date();
+
+    Requests.update({_id: request}, {open: false, time: date.getTime() - initialTime});
+
+    var hours = (Requests.findOne(request).time) / 3600000;
+    var amount = Requests.findOne(request).amount;
+
+    var transactionScore = (336 / hours) * amount;
+
+    amount = amount * Math.pow(1.04, hours);
+
+    // return money back
+
+    Requests.update({_id: request}, {score: transactionScore});
+
+    // updates users credit score
+
+    var creditScore = Profiles.findOne({user: receiver}).score + transactionScore;
+    Profiles.update({user: receiver}, {score: creditScore});
+
+  }
 
 });
