0118ae1e0244423d9bafc5852c65242701e38900
Arun Kirubarajan <arun.kirubarajan@gmail.com>
2016-10-15 17:39:37 -0400

    added email shit

--- server/main.js
+++ server/main.js
@@ -1,5 +1,40 @@
 import { Meteor } from 'meteor/meteor';
+import { Email } from 'meteor/email';
+
+var schedule = later.parse.recur().on(1).minute(); // on fifth minute of every hour, every day
+var hourlyEmailer = new ScheduledTask(schedule, function () {
+
+  var requests = Requests.find({accepted: true, open: true});
+
+  for (var i = 0; i < requests.length; i++) {
+
+    // compound the amount
+
+    var total = requests[i].total;
+
+    var difference = total * 0.04;
+
+    total = total * 1.04;
+
+    Requests.update({_id: requests[i]._id}, {total: total});
+
+    // send email to receiver that his debt's been compounded
+
+    Email.send({
+      to: "simh@live.ca",
+      from: "hello@notsoloneshark.loan",
+      subject: "Example Email",
+      text: "The contents of our email in plain text.",
+    });
+
+  }
+
+});
 
 Meteor.startup(() => {
-  // code to run on server at startup
+
+  hourlyEmailer.start();
+
+  process.env.MAIL_URL = "smtp://postmaster@mg.notsoloneshark.loan:44d133fe79b9d983debf3fe27c20317f@smtp.mailgun.org:587";
+
 });
